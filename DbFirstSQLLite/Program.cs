eb279d485359a3a7ad5f1a65e20d7742bcaa3005
FatemehTaghizadeh <Taghizadeh.f1996@gmail.com>
2021-04-30 00:30:41 +0430

    change

--- DbFirstSQLLite/Program.cs
+++ DbFirstSQLLite/Program.cs
@@ -11,51 +11,23 @@ namespace DbFirstSQLLite
         static void Main(string[] args)
         {
             using var dbcontext = new tehranContext();
-            /*var ImgName = new Image();
-            var i = dbcontext.Images.FirstOrDefault(i => i.TileId.Equals("14/10507/9917"));
-            var tile_data = i.TileData;
-            Console.WriteLine(tile_data);*/
 
 
-            var datas = dbcontext.Images.ToList();
-            string dir = @"P:\Projects\C#Projects\DbFirstSQLLite\DbFirstSQLLite\result";
+            var datas = dbcontext.Images;
+            var dir = @"P:\Projects\C#Projects\DbFirstSQLLite\DbFirstSQLLite\result";
 
             foreach (var data in datas)
             {
-                string[] dir1 = data.TileId.Split('/');
+                var tileIdParts = data.TileId.Split('/');
+                var dir1 = Path.Combine(dir, tileIdParts[0], tileIdParts[1]);
                 // If directory does not exist, create it
-                if (!Directory.Exists(dir + "\\" + dir1[0]))
+                if (!Directory.Exists(dir1))
                 {
-                    Directory.CreateDirectory(dir + "\\" + dir1[0]);
-                    CheckExit(dir + "\\" + dir1[0] + "\\" + dir1[1], dir + "\\" + dir1[0] + "\\" + dir1[1] + "\\" + dir1[2] + ".pbf", data.TileData);
-                }
-                else
-                {
-                    CheckExit(dir + "\\" + dir1[0] + "\\" + dir1[1], dir + "\\" + dir1[0] + "\\" + dir1[1] + "\\" + dir1[2] + ".pbf", data.TileData);
+                    Directory.CreateDirectory(dir1);
                 }
 
-            }
-            dbcontext.SaveChanges();
-        }
-
-        private static void CheckExit(string s1, string s2, byte[] b)
-        {
-            if (!Directory.Exists(s1))
-            {
-                Directory.CreateDirectory(s1);
-                WriteToFile(s2, b);
-            }
-            else
-            {
-                WriteToFile(s2, b);
-            }
-
-        }
-        private static void WriteToFile(string path, byte[] data)
-        {
-            using (FileStream fs = File.Create(path))
-            {
-                fs.Write(data);
+                var filePath = Path.Combine(dir1, tileIdParts[2] + ".pbf");
+                File.WriteAllBytes(filePath, data.TileData);
             }
         }
     }
